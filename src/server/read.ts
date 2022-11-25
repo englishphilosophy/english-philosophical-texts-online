e97c9c1e45fc9814d51579d26e7f616401949a0b
Amyas Merivale <merivale@gmail.com>
2022-11-25 12:18:17 +0100

    Read files locally

--- src/server/read.ts
+++ src/server/read.ts
@@ -7,30 +7,18 @@ import {
   Text
 } from '../../deps.ts'
 
-export const lexicon = async (): Promise<Lexicon> => {
-  return JSON.parse(await fetchData('lexicon')) as Lexicon
-}
+export const lexicon = (): Promise<Lexicon> => fetchData('lexicon') as Promise<Lexicon>
 
-export const flatLexicon = async (): Promise<FlatLexicon> => {
-  return JSON.parse(await fetchData('lexicon-flat')) as FlatLexicon
-}
+export const flatLexicon = (): Promise<FlatLexicon> => fetchData('lexicon-flat') as Promise<FlatLexicon>
 
-export const reducedLexicon = async (): Promise<ReducedLexicon> => {
-  return JSON.parse(await fetchData('lexicon-reduced')) as ReducedLexicon
-}
+export const reducedLexicon = (): Promise<ReducedLexicon> => fetchData('lexicon-reduced') as Promise<ReducedLexicon>
 
-export const authors = async (): Promise<Author[]> => {
-  return JSON.parse(await fetchData('authors')).texts as Author[]
-}
+export const authors = async (): Promise<Author[]> => (await fetchData('index') as { texts: Author[] }).texts
 
 export const author = async (id: string): Promise<Author | undefined> => {
   const sanitizedId = id.toLowerCase().replaceAll('.', '')
   try {
-    const response = JSON.parse(await fetchData(`html/${sanitizedId}`))
-    if (response.error) {
-      return undefined
-    }
-    return response as Author
+    return await fetchData(`html/${sanitizedId}`) as Author
   } catch {
     return undefined
   }
@@ -38,11 +26,7 @@ export const author = async (id: string): Promise<Author | undefined> => {
 
 export const text = async (id: string, type: 'html' | 'mit' = 'html'): Promise<Text | undefined> => {
   try {
-    const result = JSON.parse(await fetchData(`${type}/${id.toLowerCase().replaceAll('.', '/')}`))
-    if (result.error) {
-      return undefined
-    }
-    return result as Text
+    return await fetchData(`${type}/${id.toLowerCase().replaceAll('.', '/')}`) as Text
   } catch {
     return undefined
   }
@@ -50,20 +34,18 @@ export const text = async (id: string, type: 'html' | 'mit' = 'html'): Promise<T
 
 export const analysis = async (id: string): Promise<Analysis | undefined> => {
   try {
-    const result = JSON.parse(await fetchData(`analysis/${id.toLowerCase().replaceAll('.', '/')}`))
-    if (result.error) {
-      return undefined
-    }
-    return result as Analysis
+    return await fetchData(`analysis/${id.toLowerCase().replaceAll('.', '/')}`) as Analysis
   } catch {
     return undefined
   }
 }
 
-const fetchData = async (path: string): Promise<string> => {
-  const request = new Request(`${dataUrl}/${path}`)
-  const response = await fetch(request)
-  return await response.text()
+const fetchData = async (path: string): Promise<unknown> => {
+  try {
+    const json = await Deno.readTextFile(`./build/texts/${path}.json`)
+    return JSON.parse(json)
+  } catch {
+    const json = await Deno.readTextFile(`./build/texts/${path}/index.json`)
+    return JSON.parse(json)
+  }
 }
-
-const dataUrl = 'https://ept.deno.dev'
